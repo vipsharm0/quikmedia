802f1cdbd187eceb7257125b84bc865b90bbbde2
Vipin Sharma <vipinsharma83@gmail.com>
2022-12-27 21:17:22 +0530

    prod.ts changes

--- quickmedia/src/environments/environment.prod.ts
+++ quickmedia/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  AWS_ACCESS_KEY_ID: "AKIAQSWSUF26OZWUM3H5",
+  AWS_SECRET_ACCESS_KEY: "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",
+  AWS_REGION: "ap-south-1"
 };
